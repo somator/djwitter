cc2e0d9d416b1c0b4ae432b792c6fa64138ad155
somator <torysoma@gmail.com>
2020-07-22 01:27:02 -0500

    home, tweet detail, and hello world views.

--- tweets/views.py
+++ tweets/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse("<h1>Hello world</h1>")
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")
+
+def hello_world_view(request, *args, **kwargs):
+    print(args, kwargs)
     return HttpResponse("<h1>Hello world</h1>")
