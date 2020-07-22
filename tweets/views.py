ab76a09806a03b106c774df3bee26fded927f9d6
somator <torysoma@gmail.com>
2020-07-22 02:00:05 -0500

    REST API view for tweet detail

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
@@ -8,11 +8,23 @@ def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello world</h1>")
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API vIEW
+    Consume by Javascript or Swift or Java/iOS/Android
+    return Json data
+    """
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
     except:
-        raise Http404
-    return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
+        data['message'] = "Not found"
+        status = 404
+    return JsonResponse(data, status=status) # json.dumps content_type='application/json'
+    # return HttpResponse(f"<h1>Hello {tweet_id} - {obj.content}</h1>")
 
 def hello_world_view(request, *args, **kwargs):
     print(args, kwargs)
