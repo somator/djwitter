c9823a67447c59240428c83ecc65eaed5d8f30bf
somator <torysoma@gmail.com>
2020-07-25 01:01:18 -0500

    form submission error response

--- tweets/views.py
+++ tweets/views.py
@@ -14,11 +14,8 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_create_view(request, *args, **kwargs):
-    print(request.is_ajax())
     form = TweetForm(request.POST or None)
-    print('post data is', request.POST)
     next_url = request.POST.get("next") or None
-    print("next url", next_url)
     if form.is_valid():
         obj = form.save(commit=False)
         # do other form-related logic
@@ -28,6 +25,9 @@ def tweet_create_view(request, *args, **kwargs):
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
     return render(request, 'components/form.html', context={"form": form})
 
 def tweet_list_view(request, *args, **kwargs):
