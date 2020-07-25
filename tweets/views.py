96dbdc8e8a53c3076023590bb75180e6bb253dd5
somator <torysoma@gmail.com>
2020-07-24 23:36:09 -0500

    set request header of XML HTTP request, handle AJAX

--- tweets/views.py
+++ tweets/views.py
@@ -14,7 +14,7 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_create_view(request, *args, **kwargs):
-    print('tweet_create_view')
+    print(request.is_ajax())
     form = TweetForm(request.POST or None)
     print('post data is', request.POST)
     next_url = request.POST.get("next") or None
@@ -23,6 +23,8 @@ def tweet_create_view(request, *args, **kwargs):
         obj = form.save(commit=False)
         # do other form-related logic
         obj.save()
+        if request.is_ajax():
+            return JsonResponse({}, status=201)
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
