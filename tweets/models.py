ffb37b2b2a341927fb82728428adda785c0eb9e7
somator <torysoma@gmail.com>
2020-07-27 21:20:08 -0500

    associate tweets with users

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,14 @@
 import random
 from django.db import models
+from django.conf import settings
+
+User = settings.AUTH_USER_MODEL
 
 class Tweet(models.Model):
     # Maps to SQL data
     # id = models.AutoField(primary_key = True)
+    user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL) # tweets no longer have owner upon deletion, still exist in db
+    # user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
